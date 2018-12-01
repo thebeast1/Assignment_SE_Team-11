3590fa5a2182bb97c6b41248366e331a42346a3a
hammam98 <45409627+hammam98@users.noreply.github.com>
2018-12-01 20:54:26 +0200

    Hammam

--- SE_assignment.java
+++ SE_assignment.java
@@ -130,7 +130,8 @@ public class SE_ass {
                     break;
                 }case 2:
                 {
-                    // call your func. here
+                	sort(arr);
+                    //this code done by Hammam Ramy Attar ID : 20160325
                     break;
                 }case 3:
                 {
@@ -241,4 +242,27 @@ public class SE_ass {
 
     
     }
+    
+    public static sort (char []list)
+    {
+    	char temp;
+    	for(int i=list.length-1;i>0;i--)
+    	{
+    		for(int j=0;j<i;j++)
+    		{
+    			if (list[j]>list [j+1])
+    			{
+    				temp=list[j];
+    				list[j]=list[j+1];
+    				list[j+1]=temp;
+    			}
+    		}
+    	}
+    	
+    	
+    	
+    	
+    }
+    
+    
 }
