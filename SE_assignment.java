5348168fd937601a45e1d41feae3e956ac48f9e7
aahmedYassin <yassinahmed670@gmail.com>
2018-12-01 19:03:12 +0200

    Smallest_Prime_Number

--- SE_assignment.java
+++ SE_assignment.java
@@ -17,41 +17,6 @@ public class SE_ass {
     /**
      * @param args the command line arguments
      */
-	 public static void maximum3Elements(char arr[] ) {
-		int max1 = Integer.MIN_VALUE;
-		int max2 = Integer.MIN_VALUE;
-		int max3 = Integer.MIN_VALUE; 
-		
-		int arr1[]= new int[arr.length];
-		
-		for(int j=0;j<arr.length;j++){
-			arr1[j]=Integer.parseInt(String.valueOf(arr[j]));
-					}
-		
-		for (int i = 0; i < arr1.length; i++) {
-		    if (arr1[i] > max1) {
-		        max3 = max2;
-		        max2 = max1;
-		        max1 = arr1[i];
-		    } else if (arr1[i] > max2) {
-		        if (max1 == arr1[i]) {
-		            
-		        } else {
-		            max3 = max2;
-		            max2 = arr1[i];
-		        }
-		    } else if (arr1[i] > max3) {
-		        if (max1 == arr1[i] || max2 ==arr1[i]) {
-		           
-		        } else {
-		            max3 = arr1[i];
-		        }
-		    }
-		}
-		System.out.println("Maximum 1 =>> "+max1+"\n");
-		System.out.println("Maximum 2 =>> "+max2+"\n");
-		System.out.println("Maximum 3 =>> "+max3+"\n");
-	}
     public static void main(String[] args) {
         // TODO code application logic here
         int choice =0;
@@ -96,12 +61,7 @@ public class SE_ass {
                     break;
                 case 1:
                 {
-
-                   
-
                     // call your func. here
-                    most_repeated(arr);
-
                     break;
                 }case 2:
                 {
@@ -115,21 +75,12 @@ public class SE_ass {
                 {
                     // call your func. here
                     break;
-                }
-				case 13:
-            {
-             maximum3Elements(arr);
-              break;
-            }
-          case 14:
-            {
-             Get_Average(arr);
-              break;
-            }
-                case 18:
-                     most_repeated(arr);
-                  Get_Average(arr);
-                     break;
+                }case 5:
+		{
+		    Get_Smallest_Prime();
+		    break;
+		}
+
                 
             }
             
@@ -138,36 +89,37 @@ public class SE_ass {
     
     }
 
- public static void Get_Average(char arr[])
+    static boolean isPrime(int n) {
+        if (n%2==0&&n!=2) return false;
+        for(int i=3;i*i<=n;i+=2) {
+            if(n%i==0)
+                return false;
+        }
+        return true;
+    }
+    public static void Get_Smallest_Prime()
     {
-       //Ahmed Yassin
-        //20160041
-       double sum=0;
-      double v=0;
-       for(int i=0;i<arr.length;i++)
-           sum+=Character.getNumericValue(arr[i]);
-          v=sum/arr.length;
-       System.out.println(" Average = " +v);
- }
-    public static void most_repeated(char[]x)
-    {
-        int count=0;
-        int a=0;
-        char greater='0';
-        for(int q=0;q<x.length;q++)
-        {
-        for(int j=0;j<x.length;j++)
-        {
-        if(x[q]==x[j])
-         {count++;}
+	//Ahmed Gamal Mohamed Aly
+	//20160010
+  	    System.out.println("Please Enter size of arr: ");
+            int sz;
+            Scanner in = new Scanner(System.in);
+            sz = in.nextInt();
+            ArrayList<Integer>arr=new  ArrayList<Integer>();
+            System.out.println("Enter array elements");
+            for (int i = 0 ; i<sz; ++i)
+	    {
+            	int n = in.nextInt();
+           	 arr.add(n);
+	    }
+    	arr.sort(null);
+       for(int i=0;i<arr.size();i++)	
+       {
+    	   if (isPrime(arr.get(i)))
+    	   {    		   
+    		   System.out.println("Smallest Prime is : "+arr.get(i));
+    		   break;
+    	   }
        }
-       if(count>a)
-       { a=count;
-         greater=x[q];
-       }count=0;
-           }
-        System.out.println("The most repeated value is: "+greater);
-
-    
     }
 }
